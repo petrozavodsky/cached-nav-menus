558677d506fe7dc82c00b690378d050274911154
Vladimir <petrozavodsky@gmail.com>
2017-11-29 11:09:10 +0300

    upс

--- cached-nav-menus.php
+++ cached-nav-menus.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Caches calls to wp_nav_menu().
  */
 class Pj_Cached_Nav_Menus {
 	public static $ttl = 3600; // use 0 to cache forever (until nav menu update)
 	public static $cache_menus = array();
+	private static $option_field = 'pj-cached-nav-menus-timestamp';
 
 	public static function load() {
 		add_filter( 'pre_wp_nav_menu', array( __CLASS__, 'pre_wp_nav_menu' ), 10, 2 );
@@ -15,22 +17,25 @@ class Pj_Cached_Nav_Menus {
 	private static function _cache_key( $args ) {
 		$_args = (array) $args;
 		unset( $_args['menu'] );
+
 		return 'pj-cached-nav-menu:' . md5( json_encode( $_args ) );
 	}
 
 	private static function _timestamp() {
 		static $timestamp;
-		if ( ! isset( $timestamp ) )
-			$timestamp = get_option( 'pj-cached-nav-menus-timestamp', 0 );
+		if ( ! isset( $timestamp ) ) {
+			$timestamp = get_option( self::$option_field, false );
+		}
 
 		return $timestamp;
 	}
 
 	public static function pre_wp_nav_menu( $output, $args ) {
-		if ( ! empty( $args->menu ) )
+		if ( ! empty( $args->menu ) ) {
 			return $output;
+		}
 
-		$cache_key = self::_cache_key( $args );
+		$cache_key           = self::_cache_key( $args );
 		self::$cache_menus[] = $cache_key;
 
 		$cache = get_transient( $cache_key );
@@ -44,20 +49,22 @@ class Pj_Cached_Nav_Menus {
 	public static function maybe_cache_nav_menu( $html, $args ) {
 		$cache_key = self::_cache_key( $args );
 
-		if ( ! in_array( $cache_key, self::$cache_menus ) )
+		if ( ! in_array( $cache_key, self::$cache_menus ) ) {
 			return $html;
+		}
 
 		$cache = array(
-			'html' => $html,
+			'html'      => $html,
 			'timestamp' => time(),
 		);
 
 		set_transient( $cache_key, $cache, self::$ttl );
+
 		return $html;
 	}
 
 	public static function clear_caches() {
-		update_option( 'pj-cached-nav-menus-timestamp', time() );
+		update_option( self::$option_field, time() );
 	}
 }
 
